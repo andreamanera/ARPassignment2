e7fe34fd99916972ca70fa45abaec514290724d7
lorebene <lorebene@live.it>
2021-12-19 03:16:25 +0100

    prova a fare qualche debug

--- circular_buffer.c
+++ circular_buffer.c
@@ -1,13 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <signal.h>
 #include <strings.h>
-#include <string.h>
 #include <sys/wait.h>
 #include <time.h>
 #include <termios.h>
@@ -15,16 +8,23 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-#include <semaphore.h>
+#include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <semaphore.h>
 #include <fcntl.h>
+#include <sys/uio.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
-#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <pthread.h>
 
+#define ERROR_CHAR (-1)
+#define TRUE 1
+#define FALSE 0
+#define _SVID_SOURCE
 #define SIZE 2000000
 
 int main(int argc, char* argv[]){
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]){
 
     while (kBsize > 100000){
 
-        printf("Insert a number smaller than 100");
+        printf("Insert a number smaller than 100000");
 
         scanf("%d", & kBsize);
 
@@ -52,53 +52,47 @@ int main(int argc, char* argv[]){
 
     int num = kBsize / 0.004;
 
-    sem_t not_full;
-    sem_t not_empty;
-    pthread_mutex_t mutex;
+    typedef struct
+    {
+        char file_path[SIZE];
+        int in;
+        int out;
+        sem_t full;
+        sem_t empty;
+        sem_t mutex;
+    } Buffer;
 
-    /* Initialize not_full semaphore to a count of BUFFER_SIZE */
+    Buffer *ptr;
 
-    sem_init(&not_full, 0, SIZE);
+    int shmid;
 
-    /* Initialize not_empty semaphore to a count of 0 */
+    /* shmid is the id of the shared memory address for our buffer */
 
-    sem_init(&not_empty, 0, 0);
+    shmid = shmget(IPC_PRIVATE, sizeof(ptr->file_path), IPC_CREAT | 0666);
 
-    
+    /* get a pointer to our buffer in shared memory */
 
-    const char * shm_name = "/AOS";
+    ptr = (Buffer*) shmat(shmid, NULL, 0);
 
-    int shm_fd = shm_open(shm_name, O_CREAT | O_RDWR, 0666);
+    /* initialise the buffer */
 
-    int buf[SIZE];
+    ptr->in = 0;
+    ptr->out = 0;
+
+    /* initialise our semaphores (2nd param 1 means shared betweeen processes */
+
+    sem_init(&ptr->empty, 1, SIZE);
+    sem_init(&ptr->full, 1, 0);
+
+    // fork
 
     int id = fork();
+    
+    if (id != 0){
 
-    if(id != 0){
+        /* this is the producer process */
 
         int i;
-
-        caddr_t ptr;
-
-        if (shm_fd == 1) {
-            
-            printf("Shared memory segment failed\n");
-            exit(1);
-        }
-
-        ftruncate(shm_fd, SIZE*sizeof(int));
-
-        ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
-
-        if (ptr == MAP_FAILED) {
-            
-            printf("Map failed\n");
-            return 1;
-        }
-
-        /* Write into the memory segment */
-
-        // implement logic of sempahores and ring buffer
 
         fd_time0 = open(argv[1], O_WRONLY);
 
@@ -110,91 +104,40 @@ int main(int argc, char* argv[]){
 
         write(fd_time0, &time_taken0, sizeof(time_taken0));
 
-        int in = 0; //index at which producer will put the next data
+        int a = 0;
 
-        int count = 0;
+        while(a < num){
 
-        while(count < num){
+            a++;
 
-            // produce an item
+            sem_wait(&ptr->empty);
 
-            sem_wait(&not_full); // wait/sleep when there are no empty slots
-            pthread_mutex_lock(&mutex);
+            ptr->file_path[ptr->in] = 1 + rand()%100; 
+            ptr->in = (ptr->in + 1)%SIZE;
 
-            int item = 1 + rand()%100;
-
-            buf[in] = item;
-
-            in = (in+1) % SIZE;
-
-            count++;
-            
-            pthread_mutex_unlock(&mutex);
-            sem_post(&not_empty); // Signal/wake to consumer that buffer has some   data and they can consume now
-            
+            sem_post(&ptr->full);
         }
-
-        munmap(ptr, SIZE);
     }
 
     else{
+        
+        /* this is the consumer process */
+        
+        int b;
 
-        int i;
+        while (b < num){
 
-        caddr_t ptr;
+            b++;
 
-        shm_fd = shm_open(shm_name, O_RDONLY, 0666);
+            sem_wait(&ptr->full);
 
-        if (shm_fd == 1) {
+            int h = ptr->file_path[ptr->out];
+            ptr->out = (ptr->out + 1)%SIZE;
 
-            printf("Shared memory segment failed\n");
-            exit(1);
+            sem_post(&ptr->empty);
         }
-
-        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-
-        if (ptr == MAP_FAILED) {
-
-            printf("Map failed\n");
-            return 1;
-        }
-
-
-        int Cons[SIZE];
-        int item;
 
         fd_time1 = open(argv[2], O_WRONLY);
-
-        int out = 0; // index from which the consumer will consume next data
-
-        int count = 0;
-
-        while(count < num){
-
-            sem_wait(&not_empty); // wait/sleep when there are no empty slots
-            pthread_mutex_lock(&mutex);
-
-            printf("aaa");
-
-            fflush(stdout);
-
-            int item;
-
-            item = buf[out];
-
-            out = (out + 1) % SIZE;
-
-            count++;
-            
-            pthread_mutex_unlock(&mutex);
-            sem_post(&not_full); // Signal/wake to consumer that buffer has some   data and they can consume now
-        }
-
-
-        if (shm_unlink(shm_name) == 1) {
-
-            exit(1);
-        }
 
         seconds1 = clock();
 
@@ -203,19 +146,21 @@ int main(int argc, char* argv[]){
         printf("Time 1 : %f\n", time_taken1);
 
         write(fd_time1, &time_taken1, sizeof(time_taken1));
-
+          
     }
+
+    sem_destroy(&ptr->empty);
+    sem_destroy(&ptr->full);
+    
+    /* detach the shared memory and deallocate the memory segment */
+    
+    shmdt(&ptr);
+    shmctl(shmid, IPC_RMID, 0);
 
     close(fd_time0);
     close(fd_time1);
 
-    sem_destroy(&not_empty);
-    sem_destroy(&not_full);
+    /* finally, close the signature file */
 
-    pthread_mutex_destroy(&mutex);
-    
     return 0;
 }
-
-
-
